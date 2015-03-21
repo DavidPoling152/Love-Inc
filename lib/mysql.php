52ee7237ec3fdb879102c6e11a23d133b4f44b2f
Alex Johnson <mrjohnsonalex@gmail.com>
2015-03-21 02:22:37 -0400

    forgot to use getenv

--- lib/mysql.php
+++ lib/mysql.php
@@ -1,7 +1,7 @@
 <?php
 
-$con = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), 'OPENSHIFT_MYSQL_DB_USERNAME',
-    'OPENSHIFT_MYSQL_DB_PASSWORD', 'OPENSHIFT_APP_NAME')
+$con = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
+    getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), getenv('OPENSHIFT_APP_NAME'))
 or die("Error connecting to database");
 
 ?>
